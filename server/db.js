a8707c700aab0b875c956cffc5ed0915257923e9
freimyHidalgo77 <freimy4@gmail.com>
2026-04-10 16:31:36 -0400

    Archivo db para comunicar con la base datos

---
+++ server/db.js
@@ -0,0 +1,21 @@
+requiere(dotenv).config();
+
+const mysql = require('mysql2');4
+
+const connection = mysql.createConnection({
+
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER ||'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'docentes_db'
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.log('Error al conectar a la bd :', err);
+    }
+    console.log('Conexion exitosa....');
+
+});
+
+module.exports = connection;
