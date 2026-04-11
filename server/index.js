80840621b00e90d4d0df3b3455cbb4460d940ac8
freimyHidalgo77 <freimy4@gmail.com>
2026-04-10 20:26:43 -0400

    Agregando endpoints faltantes para funcionalidad CRUD

--- server/index.js
+++ server/index.js
@@ -46,3 +46,97 @@ app.get('/docentes:/id', (req, res) => {
    res.json(results[0]);
 
 });
+
+
+//Ruta 3: POST empleado
+app.post('/docentes', (req, res) => {
+    const {nombres, correo, telefono, titulo, area_academica, dedicacion, anios_experiencia} = req.body;
+
+    if(!nombres?.trim() || !correo?.trim() || !telefono?.trim() || !titulo?.trim() || !area_academica?.trim() || !dedicacion?.trim() || !anios_experiencia){
+        return res.status(400).json({ error: 'Todos los campos son requeridos' });
+    }
+
+     const anios = Number(anios_experiencia);
+
+     if(Number.isNaN(anios) || anios < 0){
+        return res.status(400).json({ error: 'Años de experiencia del docente invalidos' });
+     }
+
+     const sql = 'INSERT INTO docentes (nombres, correo, telefono, titulo, area_academica, dedicacion, anios_experiencia) values (?, ?, ?, ?, ?, ?, ?)';
+
+     db.query(sql, [nombres, correo, telefono, titulo, area_academica, dedicacion, anios], (err, results) => {
+        if (err) {
+            //500 error interno del servidor, fallo la bd
+            return res.status(500).json({ error: 'Error al guardar el docente' });
+        }
+
+        res.json({ 
+            id: results.insertId, 
+            nombres, correo, 
+            telefono, titulo, 
+            area_academica, 
+            dedicacion, 
+            anios_experiencia: anios 
+
+           });
+        });
+    });
+
+
+    //Ruta 4 update docente
+app.put('/docentes:/id', (req, res) => {
+
+    const { id } = req.params;
+    const {nombres, correo, telefono, titulo, area_academica, dedicacion, anios_experiencia} = req.body;
+
+    if(!nombres?.trim() || !correo?.trim() || !telefono?.trim() || !titulo?.trim() || !area_academica?.trim() || !dedicacion?.trim() || !anios_experiencia){
+        return res.status(400).json({ error: 'Todos los campos son requeridos' });
+    }
+
+     const anios = Number(anios_experiencia);
+
+     if(Number.isNaN(anios) || anios < 0){
+        return res.status(400).json({ error: 'Años de experiencia del docente invalidos' });
+     }
+
+    const sql = 'Update docentes set nombres = ?, correo = ?, telefono = ?, titulo = ?, area_academica = ?, dedicacion = ?, anios_experiencia = ? where id = ?';
+
+     db.query(sql, [nombres, correo, telefono, titulo, area_academica, dedicacion, anios, id], (err) => {
+        if (err) {
+            //500 error interno del servidor, fallo la bd
+            return res.status(500).json({ error: 'Error al actualizar al docente' });
+        }
+
+   res.json({message: 'Docente actualizado correctamente'});
+
+   });
+});
+
+
+//Ruta 5: Delete docente
+    //Ruta 4 update docente
+app.delete('/docentes:/id', (req, res) => {
+
+    const { id } = req.params;
+    const sql = 'Delete from docentes where id = ?';
+
+     db.query(sql, [id], (err) => {
+        if (err) {
+            //500 error interno del servidor, fallo la bd
+            return res.status(500).json({ error: 'Error al actualizar al docente' });
+        }
+
+   res.json({message: 'Docente eliminado correctamente'});
+
+   });
+});
+
+
+app.listen(3001, () => {
+    console.log('Servidor backend corriendo desde el puerto 3001');
+});
+
+    
+
+
+
