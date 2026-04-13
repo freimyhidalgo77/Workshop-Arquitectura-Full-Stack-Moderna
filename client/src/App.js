a5d9829572225ecc1c1c205e7e25d812e70262e9
freimyHidalgo77 <freimy4@gmail.com>
2026-04-13 14:39:26 -0400

    Agregando metodos basicos del formulario

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,50 @@
-import logo from './logo.svg';
+import { useState, useEffect, useInsertionEffect } from 'react';
 import './App.css';
 
 function App() {
 
+  //memoria del formulario
+  //crear los estados
+
+  const[nombre, setNombre] = useState("");
+  const[correo, setCorreo] = useState("");
+  const[telefono, setTelefono] = useState("");
+  const[titulo, setTitulo] = useState("");
+  const[areaAcademica, setAreaAcademica] = useState("");
+  const[dedicacion, setDedicacion] = useState("");
+  const[aniosExperiencia, setAniosExperiencia] = useState(0);
+
+  const[resgitros, setRegistros] = useState([]);
+  const[editIndex, setEditIndex] = useState(null);
+
+  useEffect(() => {
+    cargarDocentes();
+
+  },[]);
 
 
+  const cargarDocentes = asyc() => {
+    try {
+      const response = await fetch('http://localhost:3001/docentes');
+      const data = await response.json();
+      setRegistros(data);
+
+    }catch(error){
+      alert('error al cargar los docentes');
+    }
+  };
+
+  const LimpiarFormulario  = () => {
+    setNombre("");
+    setCorreo("");
+    setTelefono("");
+    setTitulo("");
+    setAreaAcademica("");
+    setDedicacion("");
+    setAniosExperiencia(0);
+  };
 
 
-  
   return (
     <div className="App">
       <header className="App-header">
